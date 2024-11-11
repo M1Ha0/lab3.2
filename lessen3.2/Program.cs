5fa35ff50a62df9bbb0a328b89e5d0ed573b46d9
пк <@DESKTOP-81SKH73>
2024-11-11 23:25:22 +0200

    Добавьте файлы проекта.

---
+++ lessen3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    Console.WriteLine("Введите N:");
+    int N = int.Parse(Console.ReadLine());
+    for (int i = 2; i <= N; i++)
+    {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
